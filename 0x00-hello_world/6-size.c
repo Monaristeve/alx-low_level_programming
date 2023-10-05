0fae9a1556640f0a106128ef2b5f7364c4f25eee
Monaristeve <monaristeve89@gmail.com>
2023-10-05 12:48:47 -0700

    Size is not grandeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-This describes the funtion for size allocations
+ *
+ *Return: 0 when everything is succesful
+ *
+ */
+int main(void)
+{printf("Size of a char: %lu bytes(s)", sizeof(char));
+	printf("Size of an int: %lu bytes(s)", sizeof(int));
+	printf("Size of a long int: %lu bytes(s)", sizeof(long int));
+	printf("Size of a long long int: %lu bytes(s)", sizeof(long long int));
+	return(0);
+}
+
