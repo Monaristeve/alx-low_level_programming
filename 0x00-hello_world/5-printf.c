0f6e6052e05cce57332419f8e42e2adb62a5b947
Monaristeve <monaristeve89@gmail.com>
2023-10-05 09:49:41 -0700

    Hello printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - the main function goes here
+ *
+ * Return: always 0
+ *
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
+
