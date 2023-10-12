5141362704a5d82e2cd64b8edba354d79294923f
Monaristeve <monaristeve89@gmail.com>
2023-10-12 03:52:41 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -4,13 +4,13 @@
  * _isupper - program entry point
  * main - a function that checks for uppercase character
  *@c:paremeter for an integer
- * Return: 1 if c is uppercase otherwise reurn 0
+ * Return: 1 if c is uppercase otherwise return 0
  *
  */
 
 int _isupper(int c)
 {
-	if (c >= 'A' && c <= 'z')
+	if (c >= 'A' && c <= 'Z')
 		return (1);
 	else
 		return (0);
