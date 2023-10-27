2bea1e098f38732630c566b864d115438f24b3a1
Monaristeve <monaristeve89@gmail.com>
2023-10-27 14:54:39 -0700

    its what they call you

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints ir=ts name followed by a new line
+ * @argc: represents number of compile line arguments
+ * @argv: represents an array that contains command line arguments
+ * Return: o
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+more argv and argc
